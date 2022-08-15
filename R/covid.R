33069882628fc27534f9a6d3e3cddebc34aa5669
dsass1 <dsass1@comcast.net>
2022-08-15 13:59:05 -0500

    update data

--- R/covid.R
+++ R/covid.R
@@ -16,18 +16,18 @@
 #'   \item{population_density}{Double; Population per squared kilometer of land area}
 #'   \item{human_development_index}{Double in the range 0 to 1; Composite index of life expectancy, education, and per capita income indicators}
 #'   \item{population_age_00_09}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_10_19}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_20_29}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_30_39}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_40_49}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_50_59}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_60_69}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_70_79}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
-#'   \item{population_age_80_and_older}{Integer; Estimated population between the ages of 0 and 9, inclusive.}
+#'   \item{population_age_10_19}{Integer; Estimated population between the ages of 10 and 19, inclusive.}
+#'   \item{population_age_20_29}{Integer; Estimated population between the ages of 20 and 29, inclusive.}
+#'   \item{population_age_30_39}{Integer; Estimated population between the ages of 30 and 39, inclusive.}
+#'   \item{population_age_40_49}{Integer; Estimated population between the ages of 40 and 49, inclusive.}
+#'   \item{population_age_50_59}{Integer; Estimated population between the ages of 50 and 59, inclusive.}
+#'   \item{population_age_60_69}{Integer; Estimated population between the ages of 60 and 69, inclusive.}
+#'   \item{population_age_70_79}{Integer; Estimated population between the ages of 70 and 79, inclusive.}
+#'   \item{population_age_80_and_older}{Integer; Estimated population ages 80 and over, inclusive.}
 #'   \item{date}{String; Time point at which vaccination values were measured. All observations taken on August 1st, 2021}
 #'   \item{new_persons_vaccinated}{Integer; Count of new persons which have received one or more doses on August 1st 2021}
-#'   \item{cumulative_persons_vaccinated}{Number of Free Throw Attempted}
-#'   \item{new_persons_fully_vaccinated}{Integer; Cumulative sum of persons which have received one or more doses}
+#'   \item{cumulative_persons_vaccinated}{Integer; Cumulative sum of persons which have received one or more doses}
+#'   \item{new_persons_fully_vaccinated}{Integer; Count of new persons which have received all doses required for maximum immunity as of August 1st 2021}
 #'   \item{cumulative_persons_fully_vaccinated}{Integer; Cumulative sum of persons which have received all doses required for maximum immunity}
 #'   \item{new_vaccine_doses_administered}{Integer; Count of vaccine doses administered to persons on August 21st 2021}
 #'   \item{cumulative_vaccine_doses_administered}{Integer; Cumulative sum of vaccine doses administered to persons}
