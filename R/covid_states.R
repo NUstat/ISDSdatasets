fc163c6935fa0e0bd2863d171b896f701e00e8ba
dsass1 <dsass1@comcast.net>
2022-08-30 16:12:08 -0500

    Add covid_states data

---
+++ R/covid_states.R
@@ -0,0 +1,25 @@
+#' Covid States
+#'
+#' @description
+#' This dataset contains daily details of COVID cases, deaths, recoveries and tests by 
+#' state in the United States for January 13, 2020 through August 22, 2022.
+#' 
+#' 
+#' @format A tibble with 72 rows and 25 variables:
+#' \describe{
+#'   \item{location_key}{Unique string identifying the region}
+#'   \item{country_code}{Country} 
+#'   \item{state_abbr}{State abbreviation}
+#'   \item{state}{State}
+#'   \item{date}{Date in the format Y:m:d}
+#'   \item{new_confirmed}{Daily count of new confirmed COVID cases.}
+#'   \item{new_deceased}{Daily count of COVID related deaths.}
+#'   \item{new_recovered}{Daily count of people recovered from COVID.}
+#'   \item{new_tested}{Daily count of number of people tested for COVID.}
+#'   \item{cumulative_confirmed}{Cumulative count of new confirmed COVID cases.}
+#'   \item{cumulative_deceased}{Cumulative count of COVID related deaths.}
+#'   \item{cumulative_recovered}{Cumulative count of people recovered from COVID.}
+#'   \item{cumulative_tested}{Cumulative count of people tested for COVID.}
+#' }
+#' @source \url{https://health.google.com/covid-19/open-data/}
+"covid_states"
--- data-raw/covid.R
+++ data-raw/covid.R
@@ -7,3 +7,13 @@ covid <-
 
 
 usethis::use_data(covid, overwrite = TRUE)
+
+
+
+covid_states <-
+  readr::read_csv(
+    file = "data-raw/covid_states.csv"
+  )
+
+
+usethis::use_data(covid_states, overwrite = TRUE)
